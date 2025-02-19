482ec0b110d7c58fe190adaf566b48bf7cecb980
AndriiSmith <asmith@awesomemotive.com>
2025-02-19 11:58:35 +0200

    Autodetect wpf on the page

--- devtools.js
+++ devtools.js
@@ -1,27 +1,27 @@
 // Function to check for WPForms element.
 function checkForWPFormsElement() {
-  chrome.devtools.inspectedWindow.eval(
-    'document.querySelector("meta[name=\'wpf-chrome-ext\']") !== null',
-    (result, isException) => {
-      if (!isException && result) {
-        // Element found, create panel.
-        chrome.devtools.panels.create(
-          "WPForms",
-          null,
-          "panel.html",
-          () => {
-            console.log("WPForms panel created.");
-          }
-        );
-      }
-    }
-  );
+	chrome.devtools.inspectedWindow.eval(
+		'document.querySelector("meta[name=\'wpf-chrome-ext\'], #wp-admin-bar-wpf-utils") !== null',
+		(result, isException) => {
+			if (!isException && result) {
+				// Element found, create panel.
+				chrome.devtools.panels.create(
+					"WPForms",
+					null,
+					"panel.html",
+					() => {
+						console.log("WPForms panel created.");
+					}
+				);
+			}
+		}
+	);
 }
 
-// Check on initial load.
+// Check on the initial load.
 checkForWPFormsElement();
 
 // Check on page reload.
 chrome.devtools.network.onNavigated.addListener(() => {
-  checkForWPFormsElement();
+	checkForWPFormsElement();
 });
